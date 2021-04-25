86993cdd6b866dc86325de3c66677664d4baeb6a
m3rashid <mdrashid.hussain.786.01@gmail.com>
2021-04-25 19:54:39 +0530

    modify

---
+++ script.js
@@ -0,0 +1,9 @@
+var signupBtn = document.getElementById("signup");
+var loginBtn = document.getElementById("login");
+
+signupBtn.addEventListener("click", ()=>{
+    window.location.href = "signup.html";
+});
+loginBtn.addEventListener("click", ()=>{
+    window.location.href = "login.html";
+});

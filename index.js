0ccbc69c6c713323cfa2f29c1f52522813a10de6
m3rashid <mdrashid.hussain.786.01@gmail.com>
2021-04-26 13:55:17 +0530

    added some js

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var div1 = document.getElementById("firstDiv");
 var div2 = document.getElementById("secondDiv");
+var email = document.getElementById("emailInstead");
 
 div1.getElementsByTagName("input")[0].addEventListener("click", ()=>{
     div1.getElementsByTagName("p")[0].style.color = "rgba(0, 191, 255)";
     div1.style.borderColor = "rgba(0, 191, 255)";
     setTimeout(()=>{
         div1.style.borderColor = "#ffffffde";
-    }, 1500);
+    }, 3000);
 });       
 
 div2.getElementsByTagName("input")[0].addEventListener("click", ()=>{
@@ -14,5 +15,23 @@ div2.getElementsByTagName("input")[0].addEventListener("click", ()=>{
     div2.style.borderColor = "rgba(0, 191, 255)";
     setTimeout(()=>{
         div2.style.borderColor = "#ffffffde";
-    }, 1500);
+    }, 3000);
 });
+
+email.addEventListener("click", ()=>{
+    if(div1.getElementsByTagName("p")[0].innerText == "Phone"){
+        email.innerText = "Use Phone Instead";
+        div1.getElementsByTagName("p")[0].innerText = "Email";
+        div1.getElementsByTagName("input")[0].setAttribute("type", "email");
+        div1.getElementsByTagName("input")[0].setAttribute("placeholder", "Email");
+    }else{
+        email.innerText = "Use Email Instead";
+        div1.getElementsByTagName("p")[0].innerText = "Phone";
+        div1.getElementsByTagName("input")[0].setAttribute("type", "number");
+        div1.getElementsByTagName("input")[0].setAttribute("placeholder", "Phone");;   
+    }
+    
+    
+    
+});
+        

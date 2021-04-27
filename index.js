1e9a7d8be47025996907ee7da5fd0e78f9ecdc7a
m3rashid <mdrashid.hussain.786.01@gmail.com>
2021-04-27 08:58:36 +0530

    minor changes

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var email = document.getElementById("emailInstead");
 
 div1.getElementsByTagName("input")[0].addEventListener("click", ()=>{
     div1.getElementsByTagName("p")[0].style.color = "rgba(0, 191, 255)";
+    div1.getElementsByTagName("input")[0].setAttribute("placeholder", "");
     div1.style.borderColor = "rgba(0, 191, 255)";
     setTimeout(()=>{
         div1.style.borderColor = "#ffffffde";
@@ -12,26 +13,27 @@ div1.getElementsByTagName("input")[0].addEventListener("click", ()=>{
 
 div2.getElementsByTagName("input")[0].addEventListener("click", ()=>{
     div2.getElementsByTagName("p")[0].style.color = "rgba(0, 191, 255)";
+    div2.getElementsByTagName("input")[0].setAttribute("placeholder", "");
     div2.style.borderColor = "rgba(0, 191, 255)";
     setTimeout(()=>{
-        div2.style.borderColor = "#ffffffde";
+        div2.style.borderColor = "#ffffff80";
     }, 3000);
 });
 
 email.addEventListener("click", ()=>{
+    var inp = div1.getElementsByTagName("input")[0];
+
     if(div1.getElementsByTagName("p")[0].innerText == "Phone"){
         email.innerText = "Use Phone Instead";
         div1.getElementsByTagName("p")[0].innerText = "Email";
-        div1.getElementsByTagName("input")[0].setAttribute("type", "email");
-        div1.getElementsByTagName("input")[0].setAttribute("placeholder", "Email");
-    }else{
+        inp.setAttribute("type", "email");
+        inp.setAttribute("placeholder", "Email");
+    }
+    else{
         email.innerText = "Use Email Instead";
         div1.getElementsByTagName("p")[0].innerText = "Phone";
-        div1.getElementsByTagName("input")[0].setAttribute("type", "number");
-        div1.getElementsByTagName("input")[0].setAttribute("placeholder", "Phone");;   
-    }
-    
-    
-    
+        inp.setAttribute("type", "number");
+        inp.setAttribute("placeholder", "Phone");;   
+    }  
 });
         

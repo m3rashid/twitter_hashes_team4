2a2617b02723f0685778db9056291454cd45dba7
m3rashid <mdrashid.hussain.786.01@gmail.com>
2021-05-01 20:54:54 +0530

    add js

--- home.js
+++ home.js
@@ -8,3 +8,36 @@ document.querySelectorAll(".go-icon")[6].addEventListener("click", ()=>{
     document.getElementById("left-pane").style.display = "block";
 });
 
+var upChevron = document.querySelector("#upChevron");
+var downChevron = document.querySelector("#downChevron");
+var msgContent = document.querySelectorAll(".msg-content");
+
+upChevron.addEventListener("click", ()=>{
+    upChevron.style.display = "none";
+    downChevron.style.display = "inline";
+    for(let i=0; i<msgContent.length; i++){
+        msgContent[i].style.display = "block";
+    }
+});
+
+downChevron.addEventListener("click", ()=>{
+    downChevron.style.display = "none";
+    upChevron.style.display = "inline";
+    for(let i=0; i<msgContent.length; i++){
+        msgContent[i].style.display = "none";
+    }
+});
+
+var inputText = document.querySelector("#what-happen").querySelectorAll("input")[0];
+
+var tweetBtn = document.querySelector("#send-tweet");
+var postValue = inputText.value;
+var createPost = document.querySelectorAll(".post")[0];
+
+tweetBtn.addEventListener("click", ()=>{
+    if(postValue != ""){
+        createPost.querySelectorAll(".post-text")[0].innerText = postValue;
+        createPost.style.display = "block"
+    }
+});
+
